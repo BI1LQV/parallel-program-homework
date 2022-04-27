0e119ab21d1d72e99854a646363d0193b8969a28
BI1LQV <qtebirdu@gmail.com>
2022-04-27 10:32:36 +0800

    原始计算 和 blas

--- homework1-gemm/main.c
+++ homework1-gemm/main.c
@@ -58,15 +58,11 @@ void gemm_OpenBlas(double *A, double *B, double *C, int m, int k, int n)
     cblas_dgemm(order, transposeA, transposeB, m, n, k, alpha, A, k, B, n, beta, C, n);
 }
 
-int main(int argc, char **argv)
+void calc(int n)
 {
     struct timeval t1, t2;
-
-    // set matrix size
-    int m = atoi(argv[1]);
-    int k = atoi(argv[2]);
-    int n = atoi(argv[3]);
-    printf("Matrix A is %i x %i, matrix B is %i x %i\n", m, k, k, n);
+    int m, k;
+    m = k = n;
     double gflop = 2.0 * (double)m * (double)n * (double)k / 1000000000.0;
 
     // malloc A, B and Cs
@@ -102,7 +98,7 @@ int main(int argc, char **argv)
     gemm_ref(A, B, C_ref, m, k, n);
     gettimeofday(&t2, NULL);
     double time_rowrow1 = (t2.tv_sec - t1.tv_sec) + (t2.tv_usec - t1.tv_usec) / 1000000.0;
-    printf("\nGEMM (row-col, A and B are in row-major)) used %4.5f s, %4.2f GFlop/s\n", time_rowrow1, gflop / time_rowrow1);
+    printf("\n%d,%4.5f, %4.2f\n", n, time_rowrow1, gflop / time_rowrow1);
 
     // check results
     int count1 = 0;
@@ -110,17 +106,34 @@ int main(int argc, char **argv)
         if (C_golden[i] != C_ref[i])
             count1++;
     if (count1 == 0)
-        printf("GEMM (row-col, A and B are in row-major) PASS!\n\n");
+        printf("\n\n");
     else
         printf("GEMM (row-col, A and B are in row-major) NOT PASS!\n\n");
 
     // the your method for GEMM
+    // memset(C_yours, 0, sizeof(double) * m * n);
+    // gettimeofday(&t1, NULL);
+    // gemm_yours(A, B, C_yours, m, k, n);
+    // gettimeofday(&t2, NULL);
+    // double time_yours = (t2.tv_sec - t1.tv_sec) + (t2.tv_usec - t1.tv_usec) / 1000000.0;
+    // printf("\n%4.5f, %4.2f\n", time_yours, gflop / time_yours);
+
+    // // check results
+    // int count2 = 0;
+    // for (int i = 0; i < m * n; i++)
+    //     if (C_golden[i] != C_yours[i])
+    //         count2++;
+    // if (count2 == 0)
+    //     printf("\n\n");
+    // else
+    //     printf("GEMM (your method) NOT PASS!\n\n");
+
     memset(C_yours, 0, sizeof(double) * m * n);
     gettimeofday(&t1, NULL);
-    gemm_yours(A, B, C_yours, m, k, n);
+    gemm_OpenBlas(A, B, C_yours, m, k, n);
     gettimeofday(&t2, NULL);
     double time_yours = (t2.tv_sec - t1.tv_sec) + (t2.tv_usec - t1.tv_usec) / 1000000.0;
-    printf("\nGEMM (your method)) used %4.5f s, %4.2f GFlop/s\n", time_yours, gflop / time_yours);
+    printf("\n%d, %4.5f , %4.2f\n", n, time_yours, gflop / time_yours);
 
     // check results
     int count2 = 0;
@@ -128,24 +141,7 @@ int main(int argc, char **argv)
         if (C_golden[i] != C_yours[i])
             count2++;
     if (count2 == 0)
-        printf("GEMM (your method) PASS!\n\n");
-    else
-        printf("GEMM (your method) NOT PASS!\n\n");
-
-    memset(C_yours, 0, sizeof(double) * m * n);
-    gettimeofday(&t1, NULL);
-    gemm_OpenBlas(A, B, C_yours, m, k, n);
-    gettimeofday(&t2, NULL);
-    time_yours = (t2.tv_sec - t1.tv_sec) + (t2.tv_usec - t1.tv_usec) / 1000000.0;
-    printf("\nGEMM (OpenBLAS)) used %4.5f s, %4.2f GFlop/s\n", time_yours, gflop / time_yours);
-
-    // check results
-    count2 = 0;
-    for (int i = 0; i < m * n; i++)
-        if (C_golden[i] != C_yours[i])
-            count2++;
-    if (count2 == 0)
-        printf("GEMM (OpenBLAS) PASS!\n\n");
+        printf("\n\n");
     else
         printf("GEMM (OpenBLAS) NOT PASS!\n\n");
 
@@ -155,4 +151,13 @@ int main(int argc, char **argv)
     free(C_golden);
     free(C_ref);
     free(C_yours);
+}
+
+int main(int argc, char **argv)
+{
+
+    for (int i = 100; i < 2000; i += 100)
+    {
+        calc(i);
+    }
 }
