6e7f9aeef086914f4dd8f532d32e5aa2de153a8a
BI1LQV <qtebirdu@gmail.com>
2022-05-21 17:03:30 +0800

    test

--- finalproject-SDNN/main.cu
+++ finalproject-SDNN/main.cu
@@ -56,7 +56,6 @@ int main(int argc, char ** argv)
     cudaMemcpy(d_A_rowpointer, A.rowpointer, (mA+1)*sizeof(int),
                cudaMemcpyHostToDevice);
 
-    size = RowPtr[m] * sizeof(int);
     cudaMalloc(&d_A_columnindex, (nnzA)*sizeof(int));
     cudaMemcpy(d_A_columnindex, A.columnindex, (nnzA)*sizeof(int),
                cudaMemcpyHostToDevice);
@@ -78,172 +77,172 @@ int main(int argc, char ** argv)
 	// free(A.columnindex);
 	// free(A.value);
 	return 0;
-	char neuronfile1[] = "neuron1024/n1024-l";
-	char neuronfile2[] = ".tsv";
-	char filename3[60];
+	// char neuronfile1[] = "neuron1024/n1024-l";
+	// char neuronfile2[] = ".tsv";
+	// char filename3[60];
 	
-	VALUE_TYPE *B0[120];
-	for (int k = 0; k < 120; k++) 
-	{	
-		char filenum[5];
-		int k1=k+1;
-		snprintf(filenum,sizeof(filenum),"%d",k1);
+	// VALUE_TYPE *B0[120];
+	// for (int k = 0; k < 120; k++) 
+	// {	
+	// 	char filenum[5];
+	// 	int k1=k+1;
+	// 	snprintf(filenum,sizeof(filenum),"%d",k1);
 		
-		strcpy(filename3, neuronfile1);
-		strcat(filename3, filenum);
-		strcat(filename3, neuronfile2);
+	// 	strcpy(filename3, neuronfile1);
+	// 	strcat(filename3, filenum);
+	// 	strcat(filename3, neuronfile2);
 
-		mmio_info(&mB, &nB, &nnzB, &isSymmetricB, filename3);
-		B[k].value=(VALUE_TYPE*)malloc((nnzB)*sizeof(VALUE_TYPE));
-		B[k].columnindex=(int*)malloc((nnzB)*sizeof(int));
-		B[k].rowpointer=(int*)malloc((mB+1)*sizeof(int));
-		mmio_data(B[k].rowpointer, B[k].columnindex, B[k].value, filename3);
+	// 	mmio_info(&mB, &nB, &nnzB, &isSymmetricB, filename3);
+	// 	B[k].value=(VALUE_TYPE*)malloc((nnzB)*sizeof(VALUE_TYPE));
+	// 	B[k].columnindex=(int*)malloc((nnzB)*sizeof(int));
+	// 	B[k].rowpointer=(int*)malloc((mB+1)*sizeof(int));
+	// 	mmio_data(B[k].rowpointer, B[k].columnindex, B[k].value, filename3);
 		
-		B0[k] = (VALUE_TYPE *)malloc(mB * nB * sizeof(VALUE_TYPE));
-        memset(B0[k], 0, sizeof(VALUE_TYPE) * mB * nB);
-		for (int i = 0; i < mB; i++)
-        {
-            for (int j = B[k].rowpointer[i]; j < B[k].rowpointer[i+1]; j++)
-            {
-                B0[k][i * nB + B[k].columnindex[j]] = B[k].value[j];
-            }
-        }
+	// 	B0[k] = (VALUE_TYPE *)malloc(mB * nB * sizeof(VALUE_TYPE));
+    //     memset(B0[k], 0, sizeof(VALUE_TYPE) * mB * nB);
+	// 	for (int i = 0; i < mB; i++)
+    //     {
+    //         for (int j = B[k].rowpointer[i]; j < B[k].rowpointer[i+1]; j++)
+    //         {
+    //             B0[k][i * nB + B[k].columnindex[j]] = B[k].value[j];
+    //         }
+    //     }
 
-		free(B[k].rowpointer);
-		free(B[k].columnindex);
-		free(B[k].value);
-	}
-	gettimeofday(&t4,NULL);
-	double time_load = (t4.tv_sec - t3.tv_sec) * 1000.0 + (t4.tv_usec - t3.tv_usec) / 1000.0;
-	printf("Weight matrix load time: %f ms \n",time_load);
+	// 	free(B[k].rowpointer);
+	// 	free(B[k].columnindex);
+	// 	free(B[k].value);
+	// }
+	// gettimeofday(&t4,NULL);
+	// double time_load = (t4.tv_sec - t3.tv_sec) * 1000.0 + (t4.tv_usec - t3.tv_usec) / 1000.0;
+	// printf("Weight matrix load time: %f ms \n",time_load);
 
-    mC = mA;
-	nC = nB;
-	// VALUE_TYPE *C0 =(VALUE_TYPE *)malloc((mC*nC)*sizeof(VALUE_TYPE));
+    // mC = mA;
+	// nC = nB;
+	// // VALUE_TYPE *C0 =(VALUE_TYPE *)malloc((mC*nC)*sizeof(VALUE_TYPE));
     
-    gettimeofday(&t3, NULL);
-	for (int k = 0; k < 120; k++) 
-	{
-		int k1=k+1;
-        // memset(C0, 0, sizeof(VALUE_TYPE)*mC*nC);
+    // gettimeofday(&t3, NULL);
+	// for (int k = 0; k < 120; k++) 
+	// {
+	// 	int k1=k+1;
+    //     // memset(C0, 0, sizeof(VALUE_TYPE)*mC*nC);
 
-		gettimeofday(&t1, NULL);
+	// 	gettimeofday(&t1, NULL);
 		
-        // for (int mi = 0; mi < mC; mi++)
-        // {
-        //     for (int ni = 0; ni < nC; ni++)
-        //     {
-        //         for (int ki = 0; ki < nA; ki++)
-        //             C0[mi * nB + ni] += A0[mi * nA + ki] * B0[k][ki * nB + ni];
-        //     }
-        // }
-		gettimeofday(&t2,NULL);
-        double time_gemm = (t2.tv_sec - t1.tv_sec) * 1000.0 + (t2.tv_usec - t1.tv_usec) / 1000.0;
+    //     // for (int mi = 0; mi < mC; mi++)
+    //     // {
+    //     //     for (int ni = 0; ni < nC; ni++)
+    //     //     {
+    //     //         for (int ki = 0; ki < nA; ki++)
+    //     //             C0[mi * nB + ni] += A0[mi * nA + ki] * B0[k][ki * nB + ni];
+    //     //     }
+    //     // }
+	// 	gettimeofday(&t2,NULL);
+    //     double time_gemm = (t2.tv_sec - t1.tv_sec) * 1000.0 + (t2.tv_usec - t1.tv_usec) / 1000.0;
 		
-		gettimeofday(&t1, NULL);
+	// 	gettimeofday(&t1, NULL);
 		
-		for (int i = 0; i < mC*nC; i++) 
-		{
-			// C0[i] += bias;
+	// 	for (int i = 0; i < mC*nC; i++) 
+	// 	{
+	// 		// C0[i] += bias;
 			
-			if (C0[i] <= 0)
-			{   
-				C0[i] = 0;
-			}
-			else if (C0[i] >= 32) 
-			{
-				C0[i] = 32;
-			}
-		}
-		gettimeofday(&t2,NULL);
-        double time_biasrelu = (t2.tv_sec - t1.tv_sec) * 1000.0 + (t2.tv_usec - t1.tv_usec) / 1000.0;
-		printf("k = %d, GEMM time: %4.5f ms, Bias+ReLU time: %4.5f ms\n", 
-		       k+1, time_gemm, time_biasrelu);
+	// 		if (C0[i] <= 0)
+	// 		{   
+	// 			C0[i] = 0;
+	// 		}
+	// 		else if (C0[i] >= 32) 
+	// 		{
+	// 			C0[i] = 32;
+	// 		}
+	// 	}
+	// 	gettimeofday(&t2,NULL);
+    //     double time_biasrelu = (t2.tv_sec - t1.tv_sec) * 1000.0 + (t2.tv_usec - t1.tv_usec) / 1000.0;
+	// 	printf("k = %d, GEMM time: %4.5f ms, Bias+ReLU time: %4.5f ms\n", 
+	// 	       k+1, time_gemm, time_biasrelu);
 
-        free(B0[k]);
+    //     free(B0[k]);
 		
-		memcpy(A0, C0, (mC*nC)*sizeof(VALUE_TYPE));
-	}
+	// 	memcpy(A0, C0, (mC*nC)*sizeof(VALUE_TYPE));
+	// }
 	
-	gettimeofday(&t4,NULL);
-	double time_inference = (t4.tv_sec - t3.tv_sec) * 1000.0 + (t4.tv_usec - t3.tv_usec) / 1000.0;
-	printf("Inference time: %f ms \n",time_inference);
+	// gettimeofday(&t4,NULL);
+	// double time_inference = (t4.tv_sec - t3.tv_sec) * 1000.0 + (t4.tv_usec - t3.tv_usec) / 1000.0;
+	// printf("Inference time: %f ms \n",time_inference);
 	
-	free(C0);
+	// free(C0);
 	
-    // check results
-	printf("test\n");
-	FILE* fs;
-	fs=fopen("sparse-images-1024-1.tsv","w+");
-	for (int i = 0; i <mA; i++)
-	{	
-		int sum =0;
-		for (int j = (i*nA); j < ((i+1)*nA); j++)
-		{  
-			sum+=A0[j];
+    // // check results
+	// printf("test\n");
+	// FILE* fs;
+	// fs=fopen("sparse-images-1024-1.tsv","w+");
+	// for (int i = 0; i <mA; i++)
+	// {	
+	// 	int sum =0;
+	// 	for (int j = (i*nA); j < ((i+1)*nA); j++)
+	// 	{  
+	// 		sum+=A0[j];
 		
-		}
-		if(sum!=0)
-		{
-			fprintf(fs,"%d\n", i+1);
-		}
-	}
-	fclose(fs);
-	FILE* fp2=NULL;
+	// 	}
+	// 	if(sum!=0)
+	// 	{
+	// 		fprintf(fs,"%d\n", i+1);
+	// 	}
+	// }
+	// fclose(fs);
+	// FILE* fp2=NULL;
 
-	fp2 = fopen("sparse-images-1024-1.tsv", "rb");
-	if (fp2 == NULL)
-	{
-		printf("Error:Open file fail!\n");
-	}
+	// fp2 = fopen("sparse-images-1024-1.tsv", "rb");
+	// if (fp2 == NULL)
+	// {
+	// 	printf("Error:Open file fail!\n");
+	// }
 
-	fseek(fp2, 0, SEEK_END);
-	size2 = ftell(fp2);
-	rewind(fp2);
+	// fseek(fp2, 0, SEEK_END);
+	// size2 = ftell(fp2);
+	// rewind(fp2);
 
-	tc2 = (int*)malloc(sizeof(int) * size2/4);
+	// tc2 = (int*)malloc(sizeof(int) * size2/4);
 
-	int readnum2 = fread(tc2, 4, size2/4, fp2);
+	// int readnum2 = fread(tc2, 4, size2/4, fp2);
 
-	fclose(fp2);
+	// fclose(fp2);
 	
-	FILE* fp1;
+	// FILE* fp1;
 
-	fp1 = fopen("neuron1024-l120-categories.tsv", "rb");
-	if (fp1 == NULL)
-	{
-		printf("Error:Open file fail!\n");
-	}
+	// fp1 = fopen("neuron1024-l120-categories.tsv", "rb");
+	// if (fp1 == NULL)
+	// {
+	// 	printf("Error:Open file fail!\n");
+	// }
 
-	fseek(fp1, 0, SEEK_END);
-	size1 = ftell(fp1);
-	rewind(fp1);
+	// fseek(fp1, 0, SEEK_END);
+	// size1 = ftell(fp1);
+	// rewind(fp1);
 
-	tc1 = (int*)malloc(sizeof(int) * size1/4);
+	// tc1 = (int*)malloc(sizeof(int) * size1/4);
 
-	int readnum1 = fread(tc1, 4, size1/4, fp1);
+	// int readnum1 = fread(tc1, 4, size1/4, fp1);
 
-	fclose(fp1);
-	int judge=0;
-	for(int i=0;i<size1/4;i++)
-	{
-		if(tc1[i]-tc2[i] != 0)
-		{
-			judge++;
-		}
-	}
-	printf("judge:%d\n",judge);
-	if (judge == 0) {
-		printf("CHALLENGE PASSED\n");
-	}
-	else
-	{
-		printf("CHALLENGE FAILED\n");
-	}
+	// fclose(fp1);
+	// int judge=0;
+	// for(int i=0;i<size1/4;i++)
+	// {
+	// 	if(tc1[i]-tc2[i] != 0)
+	// 	{
+	// 		judge++;
+	// 	}
+	// }
+	// printf("judge:%d\n",judge);
+	// if (judge == 0) {
+	// 	printf("CHALLENGE PASSED\n");
+	// }
+	// else
+	// {
+	// 	printf("CHALLENGE FAILED\n");
+	// }
 
-	free(A0);
+	// free(A0);
 
-	return 0;
+	// return 0;
 }
 
 		
